958ae0905af24ec9d38fdd718591d03ac6115165
UjjvalDhakad12 <allinonefan9@gmail.com>
2025-04-28 16:42:19 +0530

    contact page update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,43 @@
 import '../styles/Contact.css'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import axios from 'axios';
+import { useState } from 'react';
 
 const Contact = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        emial: '',
+        description: ''
+    })
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/api/contact', form, { withCredentials: true });
+            alert("Registered successfully!");
+        } catch (err) {
+            console.error(err.response?.data?.msg || "Error");
+        }
+    };
+
     return (
         <div>
             <div className='c-main'>
                 <Navbar />
-                <div className='c-card'>
+                <div>
                     <h1 className='c-title'>Contact-US</h1>
-                    <input className='c-inp' placeholder='Enter your name' />
-                    <input className='c-inp' placeholder='Enter your email' />
-                    <input className='c-inp' placeholder='write text' type='text' />
-                    <button className='c-btn'>Submit</button>
+                    <form className='c-card' onSubmit={handleSubmit}>
+                        <input name='name' className='c-inp' placeholder='Enter your name' value={form.name} onChange={handleChange} required />
+                        <input name='email' className='c-inp' placeholder='Enter your email' value={form.email} onChange={handleChange} required />
+                        <input name='description' className='c-inp' placeholder='description' type='text' value={form.description} onChange={handleChange} required />
+                        <button type='submit' className='c-btn'>Submit</button>
+                    </form>
                 </div>
             </div>
             <Footer />
